276cdc3e30776eb4b777e5b7a9a4d487bf349b63
mankaa che <mankaache10@gmail.com>
2024-01-16 17:00:46 +0100

    setting up routes

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {Home,VideoDetail,ChannelDetail,Search} from './components/pages'
+import {Navbar} from './components/layout'
+
+
+const App = () => {
+  return (
+    <Router>
+      <div className='bg-gray-800'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/Channel/:id' element={<ChannelDetail />} />
+          <Route path='/Search/:SearchTerm' element={<Search />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App
