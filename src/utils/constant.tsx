e3ef5a7d16fcbcf989db4189a1c37ba537764311
mankaa che <mankaache10@gmail.com>
2024-01-16 17:02:55 +0100

    added a utils file

---
+++ src/utils/constant.tsx
@@ -0,0 +1,45 @@
+import { IoIosMusicalNotes } from 'react-icons/io';
+import { IoHomeSharp } from 'react-icons/io5';
+import { FaCode } from 'react-icons/fa';
+import { MdMovieCreation } from 'react-icons/md';
+import { SiYoutubegaming } from 'react-icons/si';
+import { MdLiveTv } from 'react-icons/md';
+import { LiaSchoolSolid } from 'react-icons/lia';
+import { MdFaceRetouchingNatural } from 'react-icons/md';
+import { FaShirt } from 'react-icons/fa6';
+import { FaPodcast } from 'react-icons/fa6';
+import { MdTheaterComedy } from 'react-icons/md';
+import { MdSportsMartialArts } from 'react-icons/md';
+import { MdFitnessCenter } from 'react-icons/md';
+import { MdDeveloperMode } from 'react-icons/md';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <IoHomeSharp /> },
+  { name: 'JS Mastery', icon: <FaCode /> },
+  { name: 'Coding', icon: <FaCode /> },
+  { name: 'ReactJS', icon: <FaCode /> },
+  { name: 'NextJS', icon: <FaCode /> },
+  { name: 'Music', icon: <IoIosMusicalNotes /> },
+  { name: 'Education', icon: <LiaSchoolSolid /> },
+  { name: 'Podcast', icon: <FaPodcast /> },
+  { name: 'Movie', icon: <MdMovieCreation /> },
+  { name: 'Gaming', icon: <SiYoutubegaming /> },
+  { name: 'Live', icon: <MdLiveTv /> },
+  { name: 'Sport', icon: <MdSportsMartialArts /> },
+  { name: 'Fashion', icon: <FaShirt /> },
+  { name: 'Beauty', icon: <MdFaceRetouchingNatural /> },
+  { name: 'Comedy', icon: <MdTheaterComedy /> },
+  { name: 'Gym', icon: <MdFitnessCenter /> },
+  { name: 'Crypto', icon: <MdDeveloperMode /> },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle =
+  'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture =
+  'http://dergipark.org.tr/assets/app/images/buddy_sample.png';
