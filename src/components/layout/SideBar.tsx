5de8c76026fdb9e4d339bfa9c9fb7f47e481bef5
mankaa che <mankaache10@gmail.com>
2024-01-16 17:03:22 +0100

    setting up home component

---
+++ src/components/layout/SideBar.tsx
@@ -0,0 +1,42 @@
+import {categories} from '../../utils/constant'
+
+const SideBar = () => {
+
+    const selectedCategory = 'Coding';
+
+  return (
+    <section className='overflow-y-auto h-auto md:h-[95%] flex items-center justify-between md:flex-col'>
+      {categories.map((category) => (
+        <button
+          key={category.name}
+          className='category-btn text-white'
+          style={{
+            //@ts-expect-error no- error now
+            background: category.name === selectedCategory && '#fc1503',
+            color: 'white',
+          }}
+        >
+          <span
+            style={{
+              color: category.name === selectedCategory ? 'white' : 'red',
+              marginRight: '15px',
+            }}
+            className='text-xl'
+          >
+            {category.icon}
+          </span>
+          <span
+            style={{
+              opacity: category.name === selectedCategory ? '1' : '0.7',
+              marginRight: '15px',
+            }}
+          >
+            {category.name}
+          </span>
+        </button>
+      ))}
+    </section>
+  );
+}
+
+export default SideBar
