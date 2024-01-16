737c3e494aee05bd8b94c4b4fbabcffa24e395eb
mankaa che <mankaache10@gmail.com>
2024-01-16 17:02:00 +0100

    files setup

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+import  Videos  from './Videos';
+
+export {
+    Videos
+}
---
+++ src/components/layout/index.ts
@@ -0,0 +1,6 @@
+import Navbar from './Navbar';
+import SearchBar from './SearchBar'
+import SideBar from './SideBar'
+
+
+export { Navbar, SearchBar,SideBar };
