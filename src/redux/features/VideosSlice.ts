ab2bc9a3297ea68f5fef9efb11f48c64c3522806
mankaa che <mankaache10@gmail.com>
2024-01-17 12:53:07 +0100

    set up store

---
+++ src/redux/features/VideosSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    loading: false,
+    // videos: [],
+    // error: ''
+
+}
+
+export const VideoSlice = createSlice({
+    name:'video',
+    initialState,
+    reducers:{
+       
+        
+    }
+})
+
+// export const {  } = VideoSlice.actions;
---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { API } from './api';
+import { VideoSlice } from './features';
+
+export const store = configureStore({
+  reducer: {
+    [API.reducerPath]: API.reducer,
+    video: VideoSlice.reducer,
+  },
+});
+
+export type RootStateType = ReturnType<typeof store.getState>;
