85d5013eb8d9b363748776423c6f4f50d9ffd018
mankaa che <mankaache10@gmail.com>
2024-01-21 08:36:31 +0100

    channel detail page done

--- src/components/ChannelCard.tsx
+++ src/components/ChannelCard.tsx
@@ -3,31 +3,30 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constant';
 import { FaRegCircleCheck } from 'react-icons/fa6';
 
-const ChannelCard = ({channel}:any) => {
-    console.log(channel)
+const ChannelCard = ({channel,mt}:any) => {
   return (
     <div className='rounded-[20px] flex justify-center items-center w-[356px] md:w-[320px] h-[326px] m-auto'>
       <Link to={`/channel/${channel?.id?.channelId}`}>
-        <div className='flex flex-col justify-center text-center text-white'>
+        <div className={`flex w-full flex-col ${mt} justify-center text-center text-white`}>
           <div>
             <img
               src={
-                channel?.snippet?.thumbnails?.default?.url || demoProfilePicture
+                channel?.snippet?.thumbnails?.high?.url || demoProfilePicture
               }
               alt={channel?.snippet?.title}
-              className='rounded-full h-[180px] w-[180px]'
+              className='rounded-full h-[180px] w-[180px] ml-9'
             />
           </div>
-          <div className='text-3xl flex py-2 justify-start items-center gap-2'>
+          <div className='font-bold text-2xl mt-3 flex py-2 justify-start items-center gap-2'>
             {channel?.snippet?.title}
-            <FaRegCircleCheck className='text-2xl text-gray-400 ml-1' />
+            <FaRegCircleCheck className='text-xl text-primary ml-1' />
           </div>
           {
            channel?.statistics?.subscriberCount && (
-            <div>
+            <div className='font-bold text-lg'>
                 {
                     parseInt(channel?.statistics?.subscriberCount).toLocaleString()
-                } Subscribers
+                }{' '} Subscribers
             </div>
            )
           }
--- src/pages/ChannelDetail.tsx
+++ src/pages/ChannelDetail.tsx
@@ -1,9 +1,63 @@
-
+import { useParams } from 'react-router-dom'
+import { useGetChannelQuery, useGetChannelVideosQuery } from '../redux/api'
+import { setChanneldetails, allChannelVideos } from '../redux/features';
+import { useTypedDispatch } from '../redux/hooks'
+import { ChannelCard, VideoCard } from '../components';
 
 const ChannelDetail = () => {
+  const dispatch = useTypedDispatch();
+  const { id } = useParams();
+
+  const {
+    data: channelDetails,
+    isLoading,
+    error: errorDetails,
+  } = useGetChannelQuery({
+    part: 'snippet',
+    id: `${id}`,
+  });
+
+  const {
+    data: channelVideos,
+    isLoading: loadingVideos,
+    error: errorVideos,
+  } = useGetChannelVideosQuery({ channelId: `${id}`, part: 'snippet' });
+
+  //@ts-expect-error no-error
+  const details = channelDetails?.items[0];
+
+  const videos = channelVideos?.items;
+
+  if (channelDetails?.items) {
+    //@ts-expect-error no-error
+    dispatch(setChanneldetails(channelDetails?.items[0]));
+  }
+
+  if (channelVideos?.items) {
+    //@ts-expect-error no-error
+    dispatch(allChannelVideos(channelVideos?.items));
+  }
+
+  console.log(isLoading, errorDetails);
+  console.log(channelVideos?.items, loadingVideos, errorVideos);
+
   return (
-    <div>ChannelDetail</div>
-  )
+    <div className='w-full py-4'>
+      <div className='h-[300px] channelBg z-10 mt-5'>
+        <div>
+          <ChannelCard channel={details} mt='mt-80' />
+        </div>
+      </div>
+      <div className=' mx-auto flex mt-52 justify-center items-center gap-4 flex-wrap ' >
+        {videos &&
+          videos.map((item) => (
+            <div key={item.id.videoId}>
+              <VideoCard video={item} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default ChannelDetail
