13634491c2d804ebcbbc2bfea559fbfc0220bba7
mankaa che <mankaache10@gmail.com>
2024-01-22 03:42:46 +0100

    added related videos reducer

--- src/redux/features/VideosSlice.ts
+++ src/redux/features/VideosSlice.ts
@@ -2,13 +2,16 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ChannelDetails } from '../../types/channelDetails';
 import {ChannelVideos} from '../../types/channelVideos'
-
+import { VideoDetails } from '../../types/videoDetails';
+import { RelatedVideos } from '../../types/relatedVideos';
 
 interface initialStateType {
   selectedCategory: string;
   videos: Array<{ kind: string; id: any; snippet: any }>;
   setChanneldetails: ChannelDetails | null;
   channelVideos: ChannelVideos | null;
+  videoDetails: VideoDetails | null;
+  recomendedVideos: RelatedVideos | null;
 }
 
 const initialState: initialStateType = {
@@ -16,6 +19,8 @@ const initialState: initialStateType = {
   videos: [],
   setChanneldetails: null,
   channelVideos: null,
+  videoDetails: null,
+  recomendedVideos: null,
 };
 
 export const VideoSlice = createSlice({
@@ -34,8 +39,14 @@ export const VideoSlice = createSlice({
     allChannelVideos:(state,action:PayloadAction<ChannelVideos>)=>{
       state.channelVideos = action.payload
   },
+  videoDetails:(state,action:PayloadAction<VideoDetails>)=>{
+    state.videoDetails = action.payload
+  },
+  setRecommendedVideos:(state,action:PayloadAction<RelatedVideos>)=>{
+    state.recomendedVideos = action.payload
+  }
 }
 });
 
- export const { category, allChannelVideos, videos, setChanneldetails } =
+ export const { category,setRecommendedVideos, allChannelVideos, videos, videoDetails, setChanneldetails } =
    VideoSlice.actions;
