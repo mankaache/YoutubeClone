b1587b88633d028f07399e7488e1b7ee584dfc83
mankaa che <mankaache10@gmail.com>
2024-01-22 04:11:06 +0100

    fixed footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,9 +2,9 @@
 
 const Footer = () => {
   return (
-    <div className='text-white mt-3 bg-gray-800 font-bold py-2 w-full flex justify-center items-center'>
+    <div className='text-white text-xl mt-3 bg-gray-400/30 font-bold py-2 w-full flex flex-col justify-center items-center'>
       &copy; Copyright 2024 <br />{' '}
-      <span className='text-sm ml-7'>Mankaa che</span>
+      <span className='text-lg ml-7'>Mankaa che</span>
     </div>
   );
 }
