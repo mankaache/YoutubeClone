d9870aecb9f0006712e9ecc9e35b8efb029b9c84
mankaa che <mankaache10@gmail.com>
2024-01-24 10:05:39 +0100

    modified video card

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -12,19 +12,19 @@ import {
 const VideoCard = ({ video:{id: {videoId},snippet}  }: any) => {
     return (
       <>
-        <div className='w-full sm:w-[328px] md:w-[280px] rounded-lg overflow-hidden'>
+        <div className='w-full sm:w-[328px] md:w-[250px] rounded-lg overflow-hidden'>
           <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-            <div className='w-full sm:w-[328px] md:w-[280px]'>
+            <div className='w-full sm:w-[328px] md:w-[250px]'>
               <img
                 src={snippet?.thumbnails?.high?.url ? snippet?.thumbnails?.high?.url  : demoThumbnailUrl}
                 alt={snippet?.title}
               />
             </div>
           </Link>
-          <div className='bg-[#1e1e1e] h-auto min-h-[92px] px-3 py-2'>
+          <div className='bg-[#1e1e1e] h-auto min-h-[80px] px-3 py-2'>
             <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-              <div className='text-base font-bold text-white'>
-                {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0.6)}
+              <div className='text-sm font-bold text-white'>
+                {snippet?.title.slice(0, 30) + '...' || demoVideoTitle.slice(0,40)}
               </div>
             </Link>
 
@@ -35,7 +35,7 @@ const VideoCard = ({ video:{id: {videoId},snippet}  }: any) => {
                   : demoChannelUrl
               }
             >
-              <div className='flex py-2 justify-start items-center gap-2 text-sm font-bold text-gray-400'>
+              <div className='flex py-1 justify-start items-center gap-2 text-sm font-bold text-gray-400'>
                 {snippet?.channelTitle || demoChannelTitle}
                 <FaRegCircleCheck className='text-sm text-gray-400 ml-1' />
               </div>
